9e764498a6bba6011cca619cd28f46a80c44dcd8
Alexandr <scad.luncher@gmail.com>
2024-12-02 20:01:31 +0300

    local connect skeleton and new lib

--- src/jZTgUI/MainWindow.java
+++ src/jZTgUI/MainWindow.java
@@ -1,8 +1,14 @@
 package jZTgUI;
 
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 import javax.swing.JFrame;
+import org.json.*;
 
 public class MainWindow {
 
@@ -20,6 +26,34 @@ public class MainWindow {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+				
+				// https://docs.zerotier.com/api/service/ref-v1/
+				String token = "";
+				String req = "curl -H X-ZT1-Auth:"+token+" http://localhost:9993/status";
+				String[] ReqS = req.split(" ");
+				try {
+					
+					
+					// https://www.baeldung.com/java-curl
+					// https://stackoverflow.com/questions/2586975/how-to-use-curl-in-java
+					Process proc = Runtime.getRuntime().exec(ReqS);
+					InputStreamReader isr = new InputStreamReader(proc.getInputStream(), "UTF-8");
+					BufferedReader br = new BufferedReader(isr);
+					String line, resp = "";
+					while((line=br.readLine())!=null) {
+						resp += line;
+					}
+					System.out.print("Res "+ resp);
+					
+					// https://www.geeksforgeeks.org/parse-json-java/
+					//JSONObject jo = new JSONObject("");
+					
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 			}
 		});
 	}

3d6c99a757be18139d99372ac3fc9da00a56ef93
Alexandr <scad.luncher@gmail.com>
2024-12-05 13:50:14 +0300

    updated remote mechanic (for less requests)

--- jZTRep/index.php
+++ jZTRep/index.php
@@ -13,19 +13,42 @@ header('Content-Type: application/json');
 
 $ch = curl_init();
 
-$token = "";
-$fh = fopen('authtoken.secret','r');
-while ($line = fgets($fh)) {
-	$token .= $line;
+if ($_GET["apiRq"]=="controller/network_members") {
+
+	$api_rq = "controller/network/" . $_GET["netID"] . "/member";
+	curl_setopt($ch, CURLOPT_URL, "http://localhost:9993/" . $api_rq);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		'X-ZT1-Auth: ' . $_GET['token']
+	]);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// CURLOPT_RETURNTRANSFER (int)	true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
+	$netS = json_decode(curl_exec($ch), true);
+	curl_close($ch);
+
+	$output = array();
+	print("[");
+	foreach($netS as $nKey => $nVal){
+		$ch2 = curl_init();
+		curl_setopt($ch2, CURLOPT_URL, "http://localhost:9993/controller/network/" . $_GET["netID"] . "/member/" . $nKey);
+		curl_setopt($ch2, CURLOPT_HTTPHEADER, [
+			'X-ZT1-Auth: ' . $_GET['token']
+		]);
+		curl_setopt($ch2, CURLOPT_RETURNTRANSFER, false);
+		curl_exec($ch2); // will print output automaticaly, because of CURLOPT_RETURNTRANSFER=false
+		curl_close($ch2);
+	}
+	print("]");
+	//print(json_encode($output, JSON_HEX_AMP));
+
+} else {
+	curl_setopt($ch, CURLOPT_URL, "http://localhost:9993/" . $_GET["apiRq"]);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+		'X-ZT1-Auth: ' . $_GET['token']
+	]);
+	//curl_setopt($ch, CURLOPT_POST, 1);
+	//curl_setopt($ch, CURLOPT_POSTFIELDS, "postvar1=value1&postvar2=value2&postvar3=value3");
+	curl_exec($ch); // will print output automaticaly, because of CURLOPT_RETURNTRANSFER=false
+	//print(rtrim(curl_exec($ch), "1"));
+	curl_close($ch);
 }
-fclose($fh);
 
-curl_setopt($ch, CURLOPT_URL,"http://localhost:9993/".$_GET["apiRq"]);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-	'X-ZT1-Auth: '.$token
-]);
-//curl_setopt($ch, CURLOPT_POST, 1);
-//curl_setopt($ch, CURLOPT_POSTFIELDS, "postvar1=value1&postvar2=value2&postvar3=value3");
-
-print(rtrim(curl_exec($ch),"1"));
-?>
--- src/jZTgUI/jZTBridge.java
+++ src/jZTgUI/jZTBridge.java
@@ -46,7 +46,7 @@ public class jZTBridge {
 				req = "curl -H X-ZT1-Auth:" + allTokens.localToken + " " + reqAPI;
 				break;
 			case controlerToken:
-				req = "curl " + reqAPI + "&token=" + allTokens.controllerToken;
+				req = "curl " + reqAPI;
 				break;
 			case ztcentralToken:
 				req = "curl -X GET -H \"Authorization: token " + allTokens.ztCentralToken + "\" -L " + reqAPI;
@@ -97,67 +97,193 @@ public class jZTBridge {
 		return everything;
 	}
 
+	private Boolean checkNodeActive(JSONArray jNodePeers, String NodeID) {
+
+		Boolean activeNode = false;
+		for (int j = 0; j < jNodePeers.size(); j++) {
+			JSONObject jNodeProps = (JSONObject) jNodePeers.get(j);
+			String jNodeID = (String) jNodeProps.get("address");
+			if (jNodeID.startsWith(NodeID)) {
+				JSONArray jPathsAll = (JSONArray) jNodeProps.get("paths");
+				if (jPathsAll.size() > 0) {
+					JSONObject jPathOne = (JSONObject) jPathsAll.get(0);
+					activeNode = (Boolean) jPathOne.get("active");
+				}
+			}
+		}
+
+		return activeNode;
+	}
+
 	private void getNodesByID(String nodeID, String apiURL, DefaultMutableTreeNode nodeNetwork, String ContrID,
 			String LocalID, String respNetList, tokenType typeToken) {
 
 		String req;
+		JSONObject jArNetObj = new JSONObject();
+		JSONArray jPeers = new JSONArray();
 
-		JSONObject jArNetObj;
-		try {
-			jArNetObj = (JSONObject) new JSONParser().parse(respNetList);
+		if (!typeToken.equals(tokenType.ztcentralToken)) {
+			
+			req = apiURL + "peer";
 
-			Map jArNets = (Map) jArNetObj;
-			Iterator<Map.Entry> iTr = jArNets.entrySet().iterator();
-			while (iTr.hasNext()) {
-
-				Map.Entry<String, Integer> eNt = iTr.next();
-				System.out.println(eNt.getKey());
-
-				req = apiURL + "controller/network/" + nodeID + "/member/" + eNt.getKey();
-				JSONObject jNodeAsMemb = (JSONObject) new JSONParser().parse(ztAPICurl(req, typeToken));
-
-				req = apiURL + "peer/" + eNt.getKey();
-				String respJNodeProps = ztAPICurl(req, typeToken);
-				Boolean activeNode = false;
-				String crIP = "NA.NA.NA.NA";
-				if (respJNodeProps.length() > 2) {
-					JSONObject jNodeAsPeer = (JSONObject) new JSONParser().parse(respJNodeProps);
-					JSONArray jNodePropArr = (JSONArray) jNodeAsPeer.get("paths");
-					if (jNodePropArr.size() > 0) {
-						JSONObject jNodePeerProps = (JSONObject) jNodePropArr.get(0);
-						activeNode = (Boolean) jNodePeerProps.get("active");
-					}
-
-					JSONArray jNIPs = (JSONArray) jNodeAsMemb.get("ipAssignments");
-					if (jNIPs.size() > 0) {
-						crIP = (String) jNIPs.get(0);
-					}
-				} else {
-
+			String respJNodeProps = ztAPICurl(req, typeToken);
+			if (respJNodeProps.length() > 2) {
+				try {
+					jPeers = (JSONArray) new JSONParser().parse(respJNodeProps);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-
-				String authSt = "";
-				if (ContrID.startsWith(eNt.getKey())) {
-					authSt = "[C]";
-				} else if (activeNode) {
-					if (LocalID.startsWith(eNt.getKey())) {
-						authSt = "[L]";
-					} else if (activeNode) {
-						authSt = "[A]";
-					} else {
-						authSt = "[N]";
-					}
-				} else {
-					authSt = "[E]";
-				}
-				DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode(
-						crIP + " (" + eNt.getKey() + ") " + authSt);
-				nodeNetwork.add(nodeMember);
-
-				// DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode( crIP + " (" +
-				// jONodeAdv.get("name") + ") "+authSt);
-
 			}
+		}
+
+		try {
+
+			switch (typeToken) {
+			case localToken:
+
+				jArNetObj = (JSONObject) new JSONParser().parse(respNetList);
+
+				Map jArNets = (Map) jArNetObj;
+				Iterator<Map.Entry> iTr = jArNets.entrySet().iterator();
+				while (iTr.hasNext()) {
+
+					Map.Entry<String, Integer> eNt = iTr.next();
+					String peerID = eNt.getKey();
+					System.out.println(peerID);
+
+					req = apiURL + "controller/network/" + nodeID + "/member/" + peerID;
+					String respMembers = ztAPICurl(req, typeToken);
+					JSONObject jNodeAsMemb = (JSONObject) new JSONParser().parse(respMembers);
+
+					Boolean activeNode = false;
+					Boolean authrized = false;
+					String crIP = "NA.NA.NA.NA";
+					if (respMembers.length() > 2) {
+
+						activeNode = checkNodeActive(jPeers, peerID);
+						
+						authrized = (Boolean) jNodeAsMemb.get("authorized");
+
+						JSONArray jNIPs = (JSONArray) jNodeAsMemb.get("ipAssignments");
+						if (jNIPs.size() > 0) {
+							crIP = (String) jNIPs.get(0);
+						}
+
+					} else {
+
+					}
+
+					String authSt = "";
+					if (ContrID.startsWith(peerID)) {
+						authSt = "[C]";
+					} else if (authrized) {
+						if (LocalID.startsWith(peerID)) {
+							authSt = "[L]";
+						} else if (activeNode) {
+							authSt = "[A]";
+						} else {
+							authSt = "[N]";
+						}
+					} else {
+						authSt = "[E]";
+					}
+					DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode(
+							crIP + " (" + eNt.getKey() + ") " + authSt);
+					nodeNetwork.add(nodeMember);
+
+					// DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode( crIP + " (" +
+					// jONodeAdv.get("name") + ") "+authSt);
+
+				}
+				break;
+			case controlerToken:
+
+				jArNetObj = (JSONObject) new JSONParser().parse(respNetList);
+				
+				req = apiURL + "controller/network_members&netID=" + nodeID;
+				String respMembers = ztAPICurl(req, typeToken);
+				JSONArray jNodeAsMemb = (JSONArray) new JSONParser().parse(respMembers);
+
+				Map jArNets1 = (Map) jArNetObj;
+				Iterator<Map.Entry> iTr1 = jArNets1.entrySet().iterator();
+				while (iTr1.hasNext()) {
+
+					Map.Entry<String, Integer> eNt = iTr1.next();
+					String peerID = eNt.getKey();
+					System.out.println(peerID);				
+
+					String crIP = "NA.NA.NA.NA";
+					Boolean activeNode = checkNodeActive(jPeers, peerID);
+					Boolean authorized = false;
+					String authSt = "";
+					
+					if (jNodeAsMemb.size() > 0) {
+						for(int j=0; j<jNodeAsMemb.size(); j++) {
+							JSONObject jPeerObj = (JSONObject) jNodeAsMemb.get(j);
+							if(jPeerObj.get("address").toString().startsWith(peerID)) {
+								authorized = (Boolean) jPeerObj.get("authorized");
+								JSONArray ipAddr = (JSONArray) jPeerObj.get("ipAssignments");
+								if(ipAddr.size()>0) {
+									crIP = (String) ipAddr.get(0);
+								}
+							}
+						}
+					}
+					
+					if (ContrID.startsWith(peerID)) {
+						authSt = "[C]";
+					} else if (authorized) {
+						if (LocalID.startsWith(peerID)) {
+							authSt = "[L]";
+						} else if (activeNode) {
+							authSt = "[A]";
+						} else {
+							authSt = "[N]";
+						}
+					} else {
+						authSt = "[E]";
+					}
+					DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode(crIP + " (" + peerID + ") " + authSt);
+					nodeNetwork.add(nodeMember);
+
+				}
+
+				break;
+			case ztcentralToken:
+				JSONArray jArrZtc = (JSONArray) new JSONParser().parse(respNetList);
+				for (int j = 0; j < jArrZtc.size(); j++) {
+
+					JSONObject jZTCNode = (JSONObject) jArrZtc.get(j);
+					JSONObject jZTCConf = (JSONObject) jZTCNode.get("config");
+					JSONArray jZTCIps = (JSONArray) jZTCConf.get("ipAssignments");
+
+					long lastTime = (System.currentTimeMillis() - (long) jZTCNode.get("lastSeen")) / 60000;
+
+					String authSt = "";
+
+					if (LocalID.startsWith((String) jZTCNode.get("nodeId"))) {
+						authSt = "[L]";
+					} else if ((Boolean) jZTCConf.get("authorized")) {
+						if (lastTime == 0) {
+							authSt = "[A]";
+						} else {
+							authSt = "[N]";
+						}
+
+					} else {
+						authSt = "[E]";
+					}
+					DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode(jZTCIps.get(0) + " ("
+							+ jZTCNode.get("nodeId") + ") '" + jZTCNode.get("name") + "' " + authSt);
+					nodeNetwork.add(nodeMember);
+
+					System.out.println("Node " + jZTCNode.get("nodeId") + " time: "
+							+ ((System.currentTimeMillis() - (long) jZTCNode.get("lastSeen")) / 60000));
+				}
+				break;
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -171,9 +297,9 @@ public class jZTBridge {
 			Object obj;
 			JSONObject jo;
 			JSONArray jAr;
-			String req, netIP, Local_ID, Contr_ID;
+			String req, netIP = null, Local_ID, Contr_ID;
 			String localApiURL = "http://localhost:9993/";
-			String jZTRepeaterURL = allTokens.controllerApi + "?apiRq=";
+			String jZTRepeaterURL = allTokens.controllerApi + "?token=" + allTokens.controllerToken+"&apiRq=";
 			String ZTCentralAPI = "https://api.zerotier.com/api/v1/";
 
 			// https://www.geeksforgeeks.org/parse-json-java/
@@ -203,13 +329,15 @@ public class jZTBridge {
 
 					jo = (JSONObject) jAr.get(i);
 					JSONArray jArrAd = (JSONArray) jo.get("assignedAddresses");
+					String marker = "";
 					try {
 						netIP = (String) jArrAd.get(0);
 					} catch (Exception exception) {
-						netIP = "(No IP Assigned)[NAN]";
+						netIP = "<No IP Assigned>";
+						marker = " [NAN]";
 						jo.replace("name", netIP);
 					}
-					DefaultMutableTreeNode nodeNetwork = new DefaultMutableTreeNode(jo.get("name"));
+					DefaultMutableTreeNode nodeNetwork = new DefaultMutableTreeNode(jo.get("name") + "("+jo.get("id")+")"+marker);
 					System.out.println("This NET IP: " + netIP);
 
 					// https://docs.zerotier.com/controller
@@ -243,47 +371,15 @@ public class jZTBridge {
 						new JSONParser().parse(respNets);
 					} catch (ParseException eztc) {
 						// TODO: handle exception
-						//eztc.printStackTrace();
+						// eztc.printStackTrace();
 						System.out.print("Malformed ZTCenral resonse got");
 						canParse = false;
-					} 
+					}
 					if (respNets.length() > 2 && canParse) {
-						// getNodesByID((String) jo.get("id"), jZTRepeaterURL, nodeNetwork, Contr_ID, Local_ID, respNets);
-						
-						JSONArray jArrZtc = (JSONArray) new JSONParser().parse(respNets);
-						for(int j=0; j<jArrZtc.size(); j++) {
-							
-							Boolean activeNode = true;
-							
-							
-							JSONObject jZTCNode = (JSONObject) jArrZtc.get(j);
-							JSONObject jZTCConf = (JSONObject) jZTCNode.get("config");
-							JSONArray jZTCIps = (JSONArray) jZTCConf.get("ipAssignments");
-							
-							long lastTime = (System.currentTimeMillis() - (long) jZTCNode.get("lastSeen"))/60000;							
-							
-							String authSt = "";
-							
-								if (Local_ID.startsWith((String) jZTCNode.get("nodeId"))) {
-									authSt = "[L]";
-								} else if ((Boolean) jZTCConf.get("authorized")) {
-									if (lastTime == 0) {									
-										authSt = "[A]";
-									} else {										
-										authSt = "[N]";
-									}
-									
-								} else {
-									authSt = "[E]";
-								}
-							DefaultMutableTreeNode nodeMember = new DefaultMutableTreeNode(
-									jZTCIps.get(0) + " (" + jZTCNode.get("nodeId") + ") '"+jZTCNode.get("name")+"' " + authSt);
-							nodeNetwork.add(nodeMember);
-							
-							System.out.println("Node "+j+" time: " + ((System.currentTimeMillis() - (long) jZTCNode.get("lastSeen"))/60000));
-						}
-						
-						
+						// getNodesByID((String) jo.get("id"), jZTRepeaterURL, nodeNetwork, Contr_ID,
+						// Local_ID, respNets);
+						getNodesByID("", "", nodeNetwork, Contr_ID, Local_ID, respNets, tokenType.ztcentralToken);
+
 					}
 					// System.out.println(respNets.length());
 
